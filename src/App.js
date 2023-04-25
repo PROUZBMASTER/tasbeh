bfbe7401cfcee83d5d96a502f0012a2cec8cd47b
Azizbek <PROUZBMASTER>
2023-04-25 17:16:46 +0500

    fix

--- src/App.js
+++ src/App.js
@@ -16,6 +16,7 @@ function App() {
     <div className='eh'>
     <div className='box'>
       <button className='btn1' onClick={increment}>{count}</button>
+      <h1 className='arab'>مسبحة</h1>
     </div>
     <button className='btn3'onClick={rest}>restart</button>
     </div>
