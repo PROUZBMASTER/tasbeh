99a00c223ffdec1334210ef40bca0dd69ffd1be5
Azizbek <PROUZBMASTER>
2023-04-25 16:52:17 +0500

    fix

--- src/App.js
+++ src/App.js
@@ -1,24 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
-
+import React, { useState } from 'react';
+import './App.css'
 function App() {
+  const [count, setCount] = useState(0)
+  function increment (){
+    setCount(count +1)
+    if (count >= 99) {
+      setCount(0)
+    }
+  }
+
+  function rest (){
+    setCount(0)
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <>
+    <div className='box'>
+      <button className='btn1' onClick={increment}>{count}</button>
     </div>
+    <button className='btn3'onClick={rest}>restart</button>
+    </>
+      
   );
 }
 
